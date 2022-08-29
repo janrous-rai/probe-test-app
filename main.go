4be3e9e5076779a21116ce0578d88e2786993c70
Jan Rous <jan.rous@relational.ai>
2022-08-29 13:38:45 -0400

    More logic for further testing.

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 import (
     "os"
-    // "fmt"
+    "fmt"
     "net/http"
     "k8s.io/component-base/cli"
     "github.com/spf13/cobra"
@@ -38,13 +38,19 @@ func probeServer(cmd *cobra.Command, args []string) {
         Reliability: rel,
         Healthy: true,
     }
-    http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
+    healthFn := func(w http.ResponseWriter, req *http.Request) {
+        resp := 500
         if state.Healthy && (rng.Float32() < state.Reliability) {
-            w.WriteHeader(200)
-        } else {
-            w.WriteHeader(500)
+            resp = 200
         }
-    })
+        w.WriteHeader(resp)
+        fmt.Printf("health-check on %s (by %s) responded %d\n", req.URL.String(), req.UserAgent(), resp)
+    }
+    healthEps, err := cmd.Flags().GetStringArray("healthEndpoint")
+    for _, endpoint := range healthEps {
+        fmt.Printf("Serving health endpoint on /%s\n", endpoint)
+        http.HandleFunc(fmt.Sprintf("/%s", endpoint), healthFn)
+    }
     http.HandleFunc("/startoutage", func(w http.ResponseWriter, req *http.Request) {
         state.Healthy = false
         w.WriteHeader(200)
@@ -62,6 +68,7 @@ func main() {
     }
     rootCmd.AddCommand(CmdProbeServer)
     CmdProbeServer.PersistentFlags().Float32("reliability", 0.95, "target reliability for the health-probe results")
+    CmdProbeServer.PersistentFlags().StringArray("healthEndpoint", []string{"ping"}, "list of endpoints to server health on")
 
     code := cli.Run(rootCmd)
     os.Exit(code)
